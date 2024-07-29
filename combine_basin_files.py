7fb0224eaed5b4a8bfb4cb1080bafe174ee3648c
ChristinaB <cband@uw.edu>
2024-07-28 20:13:52 -0700

    Tested .py files in Spyder

--- combine_basin_files.py
+++ combine_basin_files.py
@@ -2,8 +2,9 @@
 """
 Created on Thu Jul 25 21:00:30 2024
 
-@author: Diner
+@author: a_colleage + Diner
 """
+#runfile('C:/Users/Diner/GithubRepos/DaSB-interview/combine_basin_files.py', wdir='C:/Users/Diner/USGSdata')
 
 #restart computer 
 
@@ -12,14 +13,15 @@ import glob
 import pandas as pd 
 from datetime import datetime
 
-os.chdir("C:\\Users\\a_colleague\\Desktop\\basin_prcp_scripts\\data")
+
+#os.chdir("C:\\Users\\a_colleague\\Desktop\\basin_prcp_scripts\\data")
 
 # get files from 1979-2021 
-# renaming pfile to files
+# renamed pfile to files
 
 filesALL = glob.glob('basin*')
-#Option 2: use files only to 2022
-files=filesALL[0:43]
+#Option 2: use files only to 2021
+files=filesALL[0:42]
 # Define daily data dictionary 
 daily_P={}
 # Define annual dictionary
@@ -32,15 +34,17 @@ df = pd.DataFrame(data, columns=['year', 'prcp'])
 
 for ind in index:
     current = pd.read_csv(files[ind])
-    
+    # daily time series in dictionary
     daily_P['date']=pd.to_datetime(current['day'])
     daily_P['prcp'] = round(current['prcp'], 2)
+    # annual time series in dataframe
     df.loc[ind,'year'] =daily_P['date'].dt.year[0]
     df.loc[ind,'prcp'] = round(current['prcp'].sum(),2)
 
 # fix year column by converting to integer
 df = df.astype({'year':'int'})
-"""
+
+""" your original code
 for i in range(1, 42):
     # i = 2
     year = 1978 + i 
@@ -58,8 +62,8 @@ pdata['year'] = pdata['day'].dt.year
 pdata_annual = pdata.groupby(['year']).sum()
 """
 import matplotlib.pyplot as plt
-#I recommend plotting with Year instead of index. 
+#I recommend plotting with Year instead of index and a bar instead of XY points
 #plt.plot(df.index, df['prcp'], 'o', color = 'black ##plt.plot(pdata_annual.index, pdata_annual['prcp'], 'o', color = 'black')
-plt.plot(df['year'], df['prcp'], 'o', color = 'black')
+plt.bar(df['year'], df['prcp'], color = 'blue')
 plt.xlabel('Year')
 plt.ylabel('Annual precipitation, mm')
--- subset_precip.py
+++ subset_precip.py
@@ -2,28 +2,51 @@
 """
 Created on Thu Jul 25 21:00:51 2024
 
-@author: Diner
+@author: a_colleage + Diner
 """
+#Download pr_*.nc data files
+#Manual: Go to this website and download the pr_ files first: https://www.northwestknowledge.net/metdata/data/
+#Automated: Use https://github.com/ChristinaB/DaSB-interview/blob/main/download_USGS_precip.ipynb
+
+# console code to run this file
+#runfile('C:/Users/Diner/GithubRepos/DaSB-interview/subset_precip.py', wdir='C:/Users/Diner/USGSdata')
 
 # subset nc files to our area of interest 
+import os
 import xarray as xr 
 import numpy as np 
 import pandas as pd 
+import glob
+#set I/O directory
+path_to_save_directory = os.getcwd()
+#read file
+files=glob.glob('pr_*')
+# Extract numbers from position 4 to 8
+# for first year in file name list
+start_numbers_str = files[0][3:7] # Positions are 0-indexed
+start = int(start_numbers_str)
+# for last year in file name list
+end_numbers_str = files[len(files)-1][3:7] # Positions are 0-indexed
+#Option 1: use latest year file available
+#end = int(end_numbers_str) 
+#Option 2: use 2021
+end = 2021
 
-#Go to this website and download the pr_ files first: https://www.northwestknowledge.net/metdata/data/
-
+# Set boundaries for area of interest
 lats, lons = [30, 31], [-88, -87] 
 
 # Do you specifically want to end the time series analysis at 2022? The latest files are available for 2024. Because this data catalogue is dynamic and updated annually, I updated this code so it gets the last precipitation file available and uses all available data. You can reset it to whatever start and end year data interval you want to plot.  
 
 # See code changes to line 21 and 25; yours follow commented
 
-for year in range(start, end + 1): ##for year in range(1979, 2022):
+for year in range(start, end + 1): ##for year in range(1979, 2021):
     file = 'pr_%s.nc' % year
     file_out = 'basin_prcp_%s.csv' % year 
 
     nc = xr.open_dataset(path_to_save_directory + '\\' + file) ##  nc = xr.open_dataset(local_dir + file) 
-
+    if year == 2021:
+        nc2021=nc
+        
     data = nc['precipitation_amount']
 
     data = data.loc[dict(lon = slice(lons[0], lons[1]))]
@@ -34,5 +57,4 @@ for year in range(start, end + 1): ##for year in range(1979, 2022):
     out = pd.DataFrame({'day': mean_precip['day'].values, 'prcp': mean_precip.values})
     # np.savetxt(file_out, out, delimiter=',')
     out.to_csv(file_out, index = False) 
-
-
+    
